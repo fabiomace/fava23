fa30f88a270b5921de168ca6b0bc4b27d9986e05
fabiomace <fabio.macedo@escolar.ifrn.edu.br>
2023-09-12 14:12:48 -0300

    atualização funcionando

--- src/componentes/Rodape/index.js
+++ src/componentes/Rodape/index.js
@@ -6,7 +6,6 @@ export default function Rodape(props) {
     <div className={styles.footer}>
       <footer>
         <p className={styles.copy}>@All Copyright Reserved - IFRN SGA 2023</p>
-        
         <img className={styles.imagem} src={props.imagem} alt="Twitter Logo" />
       </footer>
     </div>
