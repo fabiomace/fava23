1ed41d968f9c042dc0b08dc20c9b1f707254876b
fabiomace <117370117+fabiomace@users.noreply.github.com>
2024-09-15 17:18:18 -0300

    imagem e rodapé

--- src/componentes/Rodape/index.js
+++ src/componentes/Rodape/index.js
@@ -6,6 +6,8 @@ export default function Rodape(props) {
     <div className={styles.footer}>
       <footer>
         <p className={styles.copy}>@All Copyright Reserved - IFRN CM 2024</p>
+        <p className={styles.copy}>E-mail: fabio.macedo@escolar.ifrn.edu.br</p>
+        <p className={styles.copy}>Smartphone: (84) 98844-2443</p>
       </footer>
     </div>
   )
